a456a7764e430b0b74bdd26e0df22099b7a9a306
李静安 <3081309224@qq.com>
2025-01-21 13:29:42 +0800

    圆形进度条写成功了

--- 圆形进度条/CircularProgressBar.xaml.cs
+++ 圆形进度条/CircularProgressBar.xaml.cs
@@ -1,4 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Bai.ControlTools
+namespace Bai.WpfApp
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// CircularProgressBar.xaml 的交互逻辑
     /// </summary>
     public partial class CircularProgressBar : Window
     {
@@ -21,5 +24,4 @@ namespace Bai.ControlTools
             InitializeComponent();
         }
     }
-
 }
---
+++ 圆形进度条/LiuProgressBar.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Expression.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Bai.WpfApp
+{
+    public class LiuProgressBar : ProgressBar
+    {
+        private Arc PART_ARC;
+
+        public double Percentage
+        {
+            get { return (double)GetValue(PercentageProperty); }
+            private set { SetValue(PercentageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Percentage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentageProperty =
+            DependencyProperty.Register("Percentage", typeof(double), typeof(LiuProgressBar));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            PART_ARC = GetTemplateChild("PART_ARC") as Arc;
+            Refresh();
+        }
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+            Refresh();
+        }
+        /// <summary>
+        /// 刷新界面
+        /// </summary>
+        private void Refresh()
+        {
+            Percentage = Value / Maximum * 100;
+            if (PART_ARC != null)
+            {
+                PART_ARC.EndAngle = Percentage * 360 / 100;
+            }
+        }
+    }
+}
